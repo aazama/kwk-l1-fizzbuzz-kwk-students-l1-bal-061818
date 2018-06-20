e6b34c2e13b90df38a4fe6f9c8466527878625ed
ava azama <avaazama@gmail.com>
2018-06-20 13:06:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz (x)
-  if x % 3 ==0
+  if x % 3 ==0 && x % 5 ==0
+  puts "fizzbuzz"
+  elsif
+    x % 3 ==0
     puts "fizz"
   elsif x % 5 ==0
     puts "buzz"
-  elsif
-     x is % by {3 && 5 ==0}
-    puts "FizzBuzz"
   else
 
   end

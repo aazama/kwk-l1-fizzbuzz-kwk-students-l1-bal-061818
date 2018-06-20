22d292ca6b40987dee32cc380f15a9712c225c17
ava azama <avaazama@gmail.com>
2018-06-20 13:11:04 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz (x)
+  if x % 3 ==0 && x % 5 ==0
+  puts "fizzbuzz"
+  elsif
+    x % 3 ==0
+    puts "fizz"
+  elsif x % 5 ==0
+    puts "buzz"
+  else
+
+  end
+end
+
+fizzbuzz (3)
+fizzbuzz (5)
+fizzbuzz (15)
